126f0fb9d5ae1a1b59aa2a4764e02a421ce9a93b
tanzhong <tanzhong@163.com>
2020-06-27 19:07:04 +0800

    idea添加工程到git

---
+++ git-test-project/src/com/yzit/GitTest.java
@@ -0,0 +1,10 @@
+package com.yzit;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("enenenen");
+        System.out.println("enenenen");
+        System.out.println("enenenen");
+        System.out.println("enenenen");
+    }
+}
